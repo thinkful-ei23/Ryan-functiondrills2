8c772d3ca3998c26c489209926142c24ae98b40d
Ryan Ureta <rcureta95@gmail.com>
2018-07-16 13:01:32 -0700

    added try/catch

--- drill1.js
+++ drill1.js
@@ -1,9 +1,17 @@
-function yob(x){return 2016-x;}
+function yob(x){
+	if(x<0){throw new Error('Non-real age');}
+	if(typeof x != typeof 1){throw new Error('non-numeric value');}
+
+	return 2016-x;}
 
 
 function whoAmI(name, age){
-	var  yearOfBirth = yob(age);
+	try{var  yearOfBirth = yob(age);
 	console.log('hi my name is ' + name + ' and i\'m ' + age + ' years old');
-	console.log('I was born in ' + yearOfBirth + '.');
+	console.log('I was born in ' + yearOfBirth + '.');} 
+	catch(e){console.error(e.message);}
+
 }
 whoAmI('Ryan',29);
+whoAmI('Ryan',-4);
+whoAmI('Ryan','a');
